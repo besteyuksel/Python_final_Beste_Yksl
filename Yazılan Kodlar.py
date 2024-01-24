4c9511aff83c7d0d4ee50c6d34fa06e32ab686fe
besteyuksel <148954792+besteyuksel@users.noreply.github.com>
2024-01-24 10:11:53 +0300

    Add files via upload

---
+++ Yazılan Kodlar.py
@@ -0,0 +1,20 @@
+import processing
+# Set the input and output file paths
+input_layer_path ='C:/Users/beste/OneDrive/Desktop/workshop-20231220T101756Z 001/workshop/ankara_mahalleler.shp'
+output_layer_path='C:/Users/beste/OneDrive/Desktop/workshop-20231220T101756Z 001/workshop/Plevne_output.shp'
+# Set the buffer distance in meters
+buffer_distance = 100  # Adjust this value as needed
+# Set the attribute value to identify the specific polygon
+attribute_field = 'NAME'  # Change 'name' to the actual field name you want to use
+attribute_value = 'Plevne Mh.'  # Change 'specific_polygon' to the desired attribute value
+# Load the input layer
+input_layer = QgsVectorLayer('C:/Users/beste/OneDrive/Desktop/workshop-20231220T101756Z-001/workshop/ankara_mahalleler.shp','input_polygon','ogr')
+# Select the specific polygon based on the attribute value
+expression = f'"{attribute_field}" = \'{attribute_value}\''
+input_layer.selectByExpression(expression)
+# Create a new layer with only the selected features
+selected_layer = processing.run("native:saveselectedfeatures", {
+    'INPUT': input_layer,
+    'OUTPUT': 'memory:'})['OUTPUT']
+# Run the buffer algorithm on the selected layer
+processing.run("native:buffer", {
